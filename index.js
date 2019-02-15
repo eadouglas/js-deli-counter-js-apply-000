5aa00669cd9861d3e41dcda136f323a9c4698392
eustace douglas <eadouglas@live.co.uk>
2019-02-15 16:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 var katzDeliLine = [];
-function takeANumber(currentLine, newName) {
-   currentLine.push(newName);
-   return `Welcome, ${newName}. You are number ${currentLine.length} in line.`;
+function takeANumber(line, newName) {
+   line.push(newName);
+   return `Welcome, ${newName}. You are number ${line.length} in line.`;
 }
 
-
+function nowServing(line){
+  if (line.length === 0){ 
+    return "There is nobody waiting to be served!"
+  }
+  return "Currently serving "+ line.shift()+"."; 
+}
  
-takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
- 
-currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
- 
-nowServing(katzDeliLine); // "Currently serving Ada."
- 
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
- 
-takeANumber(katzDeliLine, "Matz"); // "Welcome, Matz. You are number 1 in line."
- 
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
- 
-nowServing(katzDeliLine); // "Currently serving Grace."
- 
-currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
+function currentLine(line){
+  if (line.length === 0){
+  return "The line is currently empty."
+  }
+  
+  return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`;
+  
+  
+}
